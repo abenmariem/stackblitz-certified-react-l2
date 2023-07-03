caa5a25581dc87120171dbcfc616851f0cf953e9
abenmariem <abderrahmen.ben.mariem@gmail.com>
2023-07-03 18:28:27 +0200

    2 files modified

--- src/QuizItemComponent.tsx
+++ src/QuizItemComponent.tsx
@@ -36,16 +36,23 @@ export const QuizItemComponent = ({ inputQuestion }: QuestionItemProps) => {
 
   const scrambledArrayAnswers: Array<string> = getScrambledArrayAnswers();
 
+  const selectAnswerHandler = (evt) => {
+       evt.preventDefault();
+       evt.target.classList.toggle("active");
+  }
+
+
   return (
     <>
       <div>
         {scrambledArrayAnswers &&
           scrambledArrayAnswers.map((answer) => (
-            <button key={answer} value={answer} className="aternativeButton">
+            <button key={answer} value={answer} className="aternativeButton" onClick={selectAnswerHandler}>
               {answer}
             </button>
           ))}
       </div>
+      <button>Submit</button>
     </>
   );
 };
